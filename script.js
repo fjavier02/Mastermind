0ba79c044bcb18d46c2a9b4b6630124ef6f05036
fjavier02 <franciscolopez1995@gmail.com>
2021-01-02 16:20:37 -0300

    star proyect Mastermind
    add firts file

---
+++ script.js
@@ -0,0 +1,75 @@
+/* alert("welcome the Mastermind")
+
+let Player1 = prompt("inserte el nombre de un jugador 1")
+let Player2 = prompt("inserte el nombre de un jugador 2")
+
+let code = prompt("por favor inserte un codigo de 4 numeros")
+while (code.split("").length !== 4) {
+    alert ('El codigo debe ser de 4 digitos')
+    let code = prompt(Player1 + "por favor inserte el codigo de 4 digitos")
+    console.log(code)
+}
+
+console.log(Player1)
+console.log(Player2)
+console.log(code) 
+
+let numero_code = code.split("")
+
+console.log(numero_code)  */
+
+/* let code1 = ["_" , "_" , "_" , "_"]
+let code2 = ["_" , "_" , "_" , "_"]
+let code3 = ["_" , "_" , "_" , "_"]
+let code4 = ["_" , "_" , "_" , "_"]
+let code5 = ["_" , "_" , "_" , "_"]
+let code6 = ["_" , "_" , "_" , "_"]
+let code7 = ["_" , "_" , "_" , "_"]
+let code8 = ["_" , "_" , "_" , "_"]
+let code9 = ["_" , "_" , "_" , "_"]
+let code10 = ["_" , "_" , "_" , "_"]
+let code11 = ["_" , "_" , "_" , "_"]
+let code12 = ["_" , "_" , "_" , "_"]
+let code13 = ["_" , "_" , "_" , "_"]
+let code14 = ["_" , "_" , "_" , "_"]
+let code15 = ["_" , "_" , "_" , "_"]
+console.log(code1.join(' '))
+console.log(code2.join(' '))
+console.log(code3.join(' '))
+console.log(code4.join(' '))
+console.log(code5.join(' '))
+console.log(code6.join(' '))
+console.log(code7.join(' '))
+console.log(code8.join(' '))
+console.log(code9.join(' '))
+console.log(code10.join(' '))
+console.log(code11.join(' '))
+console.log(code12.join(' '))
+console.log(code13.join(' '))
+console.log(code14.join(' '))
+console.log(code15.join(' ')) */
+
+
+
+
+/* for (let i = 0; i < 16; i++) {
+    console.log(i)
+  }
+ */
+/* let codeValidation = prompt(Player1 + " por favor inserte el codigo de 4 digitos")
+
+
+while (codeValidation.split("").length !== 4) {
+    
+    alert ('El codigo debe ser de 4 digitos')
+    let codeValidation = prompt(Player1 + " por favor inserte el codigo de 4 digitos")
+    console.log(codeValidation)
+}
+while (Number(codeValidation) === NaN ){
+    alert ('El codigo debe ser numerico')
+    let codeValidation = prompt(Player1 + " por favor inserte el codigo de 4 digitos")
+    console.log(codeValidation)
+}
+
+console.log(codeValidation) */
+
