2826a5f9f576eb6f309d71c6647cc1af20f3ceb5
fjavier02 <franciscolopez1995@gmail.com>
2021-01-03 15:19:53 -0300

    create validation code

--- script.js
+++ script.js
@@ -1,75 +1,157 @@
-/* alert("welcome the Mastermind")
+alert("welcome the Mastermind")
 
 let Player1 = prompt("inserte el nombre de un jugador 1")
 let Player2 = prompt("inserte el nombre de un jugador 2")
 
-let code = prompt("por favor inserte un codigo de 4 numeros")
+let code = prompt(Player1 + " por favor inserte un codigo de 4 numeros")
 while (code.split("").length !== 4) {
     alert ('El codigo debe ser de 4 digitos')
-    let code = prompt(Player1 + "por favor inserte el codigo de 4 digitos")
-    console.log(code)
+    let code = prompt(Player1 + " por favor inserte el codigo de 4 digitos")
+    console.log(code.split("").length);
+    if (code.split("").length === 4) {
+        break
+    }
 }
 
-console.log(Player1)
-console.log(Player2)
-console.log(code) 
+console.log("Jugador 1 " + Player1)
+console.log("Jugador 2 " + Player2)
+//console.log(code) 
 
 let numero_code = code.split("")
 
-console.log(numero_code)  */
+//console.log(numero_code);
 
-/* let code1 = ["_" , "_" , "_" , "_"]
-let code2 = ["_" , "_" , "_" , "_"]
-let code3 = ["_" , "_" , "_" , "_"]
-let code4 = ["_" , "_" , "_" , "_"]
-let code5 = ["_" , "_" , "_" , "_"]
-let code6 = ["_" , "_" , "_" , "_"]
-let code7 = ["_" , "_" , "_" , "_"]
-let code8 = ["_" , "_" , "_" , "_"]
-let code9 = ["_" , "_" , "_" , "_"]
-let code10 = ["_" , "_" , "_" , "_"]
-let code11 = ["_" , "_" , "_" , "_"]
-let code12 = ["_" , "_" , "_" , "_"]
-let code13 = ["_" , "_" , "_" , "_"]
-let code14 = ["_" , "_" , "_" , "_"]
-let code15 = ["_" , "_" , "_" , "_"]
-console.log(code1.join(' '))
-console.log(code2.join(' '))
-console.log(code3.join(' '))
-console.log(code4.join(' '))
-console.log(code5.join(' '))
-console.log(code6.join(' '))
-console.log(code7.join(' '))
-console.log(code8.join(' '))
-console.log(code9.join(' '))
-console.log(code10.join(' '))
-console.log(code11.join(' '))
-console.log(code12.join(' '))
-console.log(code13.join(' '))
-console.log(code14.join(' '))
-console.log(code15.join(' ')) */
+let code1 = ["_","_","_","_"];
+let code2;
+let code3;
+let code4;
+let code5;
+let code6;
+let code7;
+let code8;
+let code9;
+let code10;
+let code11;
+let code12;
+let code13;
+let code14;
+let code15;
+//console.log(code1.join(' '))
 
 
+/* for (let i = 0; i < 2; i++) {
+    let codeValidation = prompt(Player2 + " por favor inserte el codigo de 4 digitos");
+    let numero_codeValidation = codeValidation.split("");
+
+    while (codeValidation.split("").length !== 4) {
+        
+        alert ('El codigo debe ser de 4 digitos');
+        let codeValidation = prompt(Player1 + " por favor inserte el codigo de 4 digitos");
+        console.log(codeValidation);
+    }
+    while (Number(codeValidation) === NaN ){
+        alert ('El codigo debe ser numerico');
+        let codeValidation = prompt(Player1 + " por favor inserte el codigo de 4 digitos");
+        console.log(codeValidation);
+    } 
+
+     let code1 = verificacion(); 
 
 
-/* for (let i = 0; i < 16; i++) {
-    console.log(i)
-  }
- */
-/* let codeValidation = prompt(Player1 + " por favor inserte el codigo de 4 digitos")
+    console.log(code1.join(' '))
+    console.log(code2.join(' '))
+    console.log(code3.join(' '))
+    console.log(code4.join(' '))
+    console.log(code5.join(' '))
+    console.log(code6.join(' '))
+    console.log(code7.join(' '))
+    console.log(code8.join(' '))
+    console.log(code9.join(' '))
+    console.log(code10.join(' '))
+    console.log(code11.join(' '))
+    console.log(code12.join(' '))
+    console.log(code13.join(' '))
+    console.log(code14.join(' '))
+    console.log(code15.join(' '))
+} */
+
+let codeValidation = prompt(Player2 + " por favor inserte el codigo de 4 digitos");
+let numero_codeValidation = codeValidation.split("");
+verificacion();
 
 
-while (codeValidation.split("").length !== 4) {
-    
-    alert ('El codigo debe ser de 4 digitos')
-    let codeValidation = prompt(Player1 + " por favor inserte el codigo de 4 digitos")
-    console.log(codeValidation)
+// verificación de los datos
+let numero_correcto;
+let posicion_correcta;
+
+
+
+function verificacion() {
+    let numero_correcto = 0;
+    let posicion_correcta = 0;
+    if (numero_codeValidation[0] === numero_code[0]) { //verificando posicion 0
+        posicion_correcta++;
+    }else { // verificando position 1
+        if (numero_codeValidation[0] === numero_code[1]) {
+            numero_correcto++;
+        }else { // verificando position 2
+            if (numero_codeValidation[0] === numero_code[2]) {
+                numero_correcto++;
+            }else { // verificando position 3
+                if (numero_codeValidation[0] === numero_code[3]) {
+                    numero_correcto++;
+                }
+            }
+        }
+    }
+    if (numero_codeValidation[1] === numero_code[1]) { //verificando posicion 0
+        posicion_correcta++;
+    }else { // verificando position 1
+        if (numero_codeValidation[1] === numero_code[0]) {
+            numero_correcto++;
+        }else { // verificando position 2
+            if (numero_codeValidation[1] === numero_code[2]) {
+                numero_correcto++;
+            }else { // verificando position 3
+                if (numero_codeValidation[1] === numero_code[3]) {
+                    numero_correcto++;
+                }
+            }
+        }
+    }
+    if (numero_codeValidation[2] === numero_code[2]) { //verificando posicion 0
+        posicion_correcta++;
+    }else { // verificando position 1
+        if (numero_codeValidation[2] === numero_code[0]) {
+            numero_correcto++;
+        }else { // verificando position 2
+            if (numero_codeValidation[2] === numero_code[1]) {
+                numero_correcto++;
+            }else { // verificando position 3
+                if (numero_codeValidation[2] === numero_code[3]) {
+                    numero_correcto++;
+                }
+            }
+        }
+    }
+    if (numero_codeValidation[3] === numero_code[3]) { //verificando posicion 0
+        posicion_correcta++;
+    }else { // verificando position 1
+        if (numero_codeValidation[3] === numero_code[0]) {
+            numero_correcto++;
+        }else { // verificando position 2
+            if (numero_codeValidation[3] === numero_code[1]) {
+                numero_correcto++;
+            }else { // verificando position 3
+                if (numero_codeValidation[3] === numero_code[2]) {
+                    numero_correcto++;
+                }
+            }
+        }
+    }
+    //console.log(numero_correcto+ " numeros correctos");
+    //console.log(posicion_correcta+ " numeros en la posicion correcta");
+    //console.log(code1.join(' '))
+    console.log(numero_codeValidation.join(' ') + ": " + numero_correcto + " Numero correcto " + posicion_correcta + " posición correcta")
+    return numero_correcto + posicion_correcta
 }
-while (Number(codeValidation) === NaN ){
-    alert ('El codigo debe ser numerico')
-    let codeValidation = prompt(Player1 + " por favor inserte el codigo de 4 digitos")
-    console.log(codeValidation)
-}
-
-console.log(codeValidation) */
-
