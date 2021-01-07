9b7f1d394792f2749985d368d395d8dc198c6678
fjavier02 <franciscolopez1995@gmail.com>
2021-01-07 20:54:18 -0300

    full function

--- script.js
+++ script.js
@@ -1,95 +1,59 @@
-function Start() {
-    alert("welcome the Mastermind")
+alert("welcome the Mastermind")
 
-    let Player1 = prompt("inserte el nombre de un jugador 1")
-    let Player2 = prompt("inserte el nombre de un jugador 2")
+var Player1 = prompt("inserte el nombre de un jugador 1")
+var Player2 = prompt("inserte el nombre de un jugador 2")
 
-    let code = prompt(Player1 + " por favor inserte un codigo de 4 numeros")
-    while (code.split("").length !== 4) {
-        alert ('El codigo debe ser de 4 digitos')
-        let code = prompt(Player1 + " por favor inserte el codigo de 4 digitos")
-        console.log(code.split("").length);
-        if (code.split("").length === 4) {
-            break
-        }
-    }
+var code = prompt(Player1 + " por favor inserte un codigo de 4 numeros")
 
-    console.log("Jugador 1 " + Player1)
-    console.log("Jugador 2 " + Player2)
-    console.log(code) 
+var save_code = code;
 
-    let numero_code = code.split("")
+console.log("Jugador 1 " + Player1);
+console.log("Jugador 2 " + Player2);
+console.log(code) ;
+
+var numero_code = code.split("");
+
+var codeValidation = prompt("Por favor inserte el codigo de 4 digitos");
+var res = codeValidation.split("");
+console.log(res);
+verificacion();
 
 
-    /* while (Number(codeValidation) === NaN ){
-            alert ('El codigo debe ser numerico');
-            let codeValidation = prompt(Player1 + " por favor inserte el codigo de 4 digitos");
-            console.log(codeValidation);
-        } 
-    } */
 
-//    for (let i = 0; i < 4; i++) {
-        let codeValidation = prompt(Player2 + " por favor inserte el codigo de 4 digitos");
-        let validation = codeValidation.split("");
-        verificacion();
-//    }
+// verificación de los datos
+var numero_correcto;
+var posicion_correcta; 
+
+function verificacion() {
+    
+    var numero_correcto = 0;
+    var posicion_correcta = 0;
     
 
-
-    // verificación de los datos
-    let numero_correcto;
-    let posicion_correcta; 
-
-    function verificacion() {
-        
-        let numero_correcto = 0;
-        let posicion_correcta = 0;
-        let cont = [];
-        let cont2= [];
-        let save_numero_code;
-        //let save_numero_code = numero_code;
-
-        // verificación diagonal principal
-        for (let i = 0; i < 4; i++) {
-            for (let j = 0; j < 4; j++) {
-                if (i == j && validation [j] === numero_code[i] ) {
-                    posicion_correcta++;
-                    cont.push(i);
-                }       
-            }
+    // verificación diagonal principal
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (i == j && res [j] === numero_code[i] ) {
+                posicion_correcta++;
+                console.log(numero_code[i]);
+                numero_code[i]= "N";
+            }       
         }
-
-        //indice en los que los numero no estan correctos
-        for (let i = 0; i < 4; i++) {
-            if (cont.indexOf(i) == -1){
-                 cont2.push(i)
-            }
-        }
-        // intento de verificación para numero que no fueron verificados
-        for (const iterator of cont2) {
-            //console.log(numero_code[iterator]);
-            console.log(cont2); 
-            for (let j = cont2[i] ; j < 4; j++) {
-                console.log(validation [j] + " validation J: " + j );
-                console.log(numero_code[iterator] + " numero_code en " + iterator);
-                if (validation [j] === numero_code[iterator]) {
-                    //cont2.shift();
-                    numero_correcto++;
-                    console.log(numero_correcto + " se sumo 1 en numero_correcto");
-                }
-            }
-        }
-        console.log(cont2);
-
-        //proximamente solo en cines: verificación de numero_correcto
-        /* cont2.forEach(element => {
-            console.log(element)
-        }); */
-        //console.log(numero_code);
-        //console.log(save_numero_code);
-
-        let numeros_correcto = numero_correcto + posicion_correcta;
-        console.log(validation.join(' ') + ": " + numeros_correcto + " numeros correctos: " + posicion_correcta + " en la posición correcta y " + numero_correcto + " numero en la posicion incorrecta.")
-        return posicion_correcta
     }
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (i !== j && res [j] === numero_code[i] ) {
+                numero_correcto++;
+                numero_code[i]= "B";
+                console.log(numero_code[i]);
+            }       
+        }
+    }
+
+    var code = save_code;
+
+    var numeros_correcto = numero_correcto + posicion_correcta;
+    console.log(res.join(' ') + ": " + numeros_correcto + " numeros correctos: " + posicion_correcta + " en la posición correcta y " + numero_correcto + " numero en la posicion incorrecta.")
+    return posicion_correcta
 }
